a4dbbf6ed567ed8fa894bb66df3ede0c8b51493c
Stellareign <tuguran@mail.ru>
2022-10-31 21:19:42 +0300

    HW-7, часть 2, цикл for

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Домашка 7 - циклы - задача 1");
-        for (int i = 0; i <= 10; i++) {
+        System.out.println("Домашка 7 - циклы, часть вторя - задача 1, високосный год");
+        for (int i = 1904; i <= 2096 && i >=1904; i= i + 4) {
             {
-                System.out.print(" " + i + "; ");
+                System.out.print( i + " год является високосным; ");
             }
         }
         System.out.println("" +
                 "");
-            System.out.println("Домашка 7 - циклы - задача 2");
-            for (int i = 10; i >= 0; i--) {
-                {
-                    System.out.print(" " + i + "; ");
+        System.out.println("Домашка 7 - циклы, часть вторя - задача 2, арифметическая последовательность +7");
+        for (int i = 7; i >= 7 && i <= 98; i = i + 7) {
+            System.out.print(" " + i + "; ");
+        }
+        System.out.println("" +
+                "");
+        System.out.println("Домашка 7 - циклы, часть вторя - задача 3, последовательность i+i");
+        for (int i = 1; i >= 1 && i <= 512; i = i+i) {
+            System.out.print(" " + i + "; ");
+        }
                 }
-            }
-        System.out.println("" +
-                "");
-        System.out.println("Домашка 7 - циклы - задача 3");
-        for (int i = 0; i <= 17; i = (i + 2)) {
-            {
-                System.out.print(" " + i + "; ");
-            }
         }
-        System.out.println("" +
-                "");
-        System.out.println("Домашка 7 - циклы - задача 4");
-        for (int i = 10; i >= -10; i--) {
-            {
-                System.out.print(" " + i + "; ");
-            }
-        }
-        }
-    }
+
