f8545af6a0befebd530df081991d9f90892ffd63
Stellareign <tuguran@mail.ru>
2022-10-31 21:04:37 +0300

    HW-7, цикл for

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Домашка 7 - циклы - задача 1");
+        for (int i = 0; i <= 10; i++) {
+            {
+                System.out.print(" " + i + "; ");
+            }
+        }
+        System.out.println("" +
+                "");
+            System.out.println("Домашка 7 - циклы - задача 2");
+            for (int i = 10; i >= 0; i--) {
+                {
+                    System.out.print(" " + i + "; ");
+                }
+            }
+        System.out.println("" +
+                "");
+        System.out.println("Домашка 7 - циклы - задача 3");
+        for (int i = 0; i <= 17; i = (i + 2)) {
+            {
+                System.out.print(" " + i + "; ");
+            }
+        }
+        System.out.println("" +
+                "");
+        System.out.println("Домашка 7 - циклы - задача 4");
+        for (int i = 10; i >= -10; i--) {
+            {
+                System.out.print(" " + i + "; ");
+            }
+        }
+        }
+    }
