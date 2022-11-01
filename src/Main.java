5ac015fe55dda96135c35214fb915a11c4309261
Stellareign <tuguran@mail.ru>
2022-11-01 20:56:32 +0300

    HW-7, часть 3, цикл for

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,17 @@ public class Main {
         saving = savingMonth + saving;
             System.out.print("Месяц " + i + " - сумма накоплений равна " + saving + ". ");
         }
+        System.out.println("" +
+                ""+"" +
+                "");
+        System.out.println("Домашка 7, часть 3, задача 2, накопления с процентами");
+        int deposit = 29_000;
+        double saving2 = 0;
+        double percentMonth = 1.01;
+        for (int i = 1; i <= 12; i++) {
+            saving2 = deposit + saving2*percentMonth;
+            System.out.print("Месяц " + i + " - сумма накоплений равна " + saving2 + ". ");
+        }
     }
 }
 
